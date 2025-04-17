11bd3f42fc7e5d52bb986ef6ca39462cbdaa30b9
okhgoa123 <johntut097@gmail.com>
2025-04-16 21:45:20 -1000

    Yers

--- EriksonProject/timeline.js
+++ EriksonProject/timeline.js
@@ -9,18 +9,27 @@ var mouseX
 var mouseY
 var windowHasLoaded = false;
 
+var widgetPicList = ["Photos/Image304.jpg"]
 
 function assigner()
 {
     windowHasLoaded = true
     stage1 = document.getElementById("one")
-    stageBoxes = [stage1//, stage2, stage3, stage4
-    ]
+    stageBoxes = [stage1 ]
 }
 
 window.onload = assigner
 
 var stageBoxes 
+
+function flushWidgets()
+{
+    var widgets = document.getElementsByClassName("widget")
+        for (var j = 0; j < widgets.length; j++)
+        {
+            widgets[j].remove()
+        }
+}
 
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -34,21 +43,48 @@ document.addEventListener('mousemove', function(event) {
 
 setInterval(() => {
     if (hovered == false && windowHasLoaded == true)
-        {
+    {
             for (var i = 0; i < stageBoxes.length; i++)
             {
-                console.log(stageBoxes[i].matches(":hover"))
-                if (stageBoxes[i].matches(":hover"))
+                console.log(stageBoxes[i])
+                console.log(stageBoxes[i].matches("div:hover"))
+                if (stageBoxes[i].matches("div:hover"))
                 {
                     console.log("HOVERING")
                     var widget = document.createElement("div")
+                    var widgetImage = document.createElement("div")
                     widget.setAttribute("class", "widget")
+                    widgetImage.setAttribute("class", "widgetPic")
+                    widgetImage.style.backgroundImage = "url('" + widgetPicList[i] + "')"
                     widget.style.left = mouseX + "px"
-                    widget.style.top = mouseY = "px"
+                    widget.style.top = (mouseY + 3) + "px"
+                    widget.appendChild(widgetImage)
                     document.body.appendChild(widget)
+                    console.log(widget)
+                    hovered = true 
+                    break
                 }
+                //flush all widgets if not
+                flushWidgets()
+            }
+    }
+    //when hover is true, continually check to see if still hovering
+    if (hovered == true)
+    {
+        for (var i = 0; i < stageBoxes.length; i++)
+        {
+            if (stageBoxes[i].matches("div:hover"))
+            {
+                break
+            }
+            if (i == stageBoxes.length - 1)
+            {
+                //when hover is false, delete all widgets
+                hovered = false
+                flushWidgets()
             }
         }
+    }
 }, 200);
 
 
